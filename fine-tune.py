703b3abd40e10def51956518703a576707188aa4
BevisD <bevisd@btinternet.com>
2024-02-29 16:00:37 +0000

    New json format

--- fine-tune.py
+++ fine-tune.py
@@ -1,13 +1,50 @@
+import argparse
+
 import torch
 from monai.networks.nets import SwinUNETR
 from monai.losses import DiceCELoss
 from monai.transforms import AsDiscrete
 from monai.metrics import DiceMetric
 
-from utils import post_pred_transform, SwinInferer, get_train_loader, get_test_loader
+from utils import post_pred_transform, SwinInferer, get_train_loader
 from trainer import run_training
 
-IMG_SIZE = 96
+parser = argparse.ArgumentParser()
+# Model Architecture
+parser.add_argument("roi-size", type=int, default=96)
+parser.add_argument("in-channels", type=int, default=1)
+parser.add_argument("out-channels", type=int, default=2)
+parser.add_argument("feature-size", type=int, default=48)
+
+# Training Hyperparameters
+parser.add_argument("drop-rate", type=float, default=0.0)
+parser.add_argument("attn-drop-rate", type=float, default=0.0)
+parser.add_argument("path-drop-rate", type=float, default=0.0)
+parser.add_argument("grad-checkpoint", action="store_true")
+parser.add_argument("square-pred", action="store_true")
+parser.add_argument("learning-rate", type=float, default=1e-4)
+parser.add_argument("weight-decay", type=float, default=1e-5)
+parser.add_argument("max-epochs", type=int, required=True)
+parser.add_argument("batch-size", type=int, default=1)
+parser.add_argument("val-every", type=int, default=4)
+
+# Transform Parameters
+parser.add_argument("a-min", type=float, default=-1220)
+parser.add_argument("a-max", type=float, default=3500)
+parser.add_argument("b-min", type=float, default=0)
+parser.add_argument("b-max", type=float, default=1)
+parser.add_argument("b-max", type=float, default=1)
+parser.add_argument("space-x", type=float, default=1.0)
+parser.add_argument("space-y", type=float, default=1.0)
+parser.add_argument("space-z", type=float, default=1.0)
+
+# Paths
+parser.add_argument("data-dir", type=str, required=True)
+parser.add_argument("json-list", type=str, required=True)
+parser.add_argument("log-dir", type=str, required=True)
+parser.add_argument("pretrained-path", type=str, required=True)
+
+roi_size = 96
 IN_CHANNELS = 1
 OUT_CHANNELS = 2
 FEATURE_SIZE = 48
@@ -18,86 +55,93 @@ USE_CHECKPOINT = True
 SQUARED_PRED = False
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 1e-5
-MAX_EPOCHS = 100
+MAX_EPOCHS = 2
 BATCH_SIZE = 4
-VAL_EVERY = 4
+VAL_EVERY = 1
 A_MIN = -1220
 A_MAX = 3500
 B_MIN = 0
 B_MAX = 1
-PIXDIM = (1.5, 1.5, 2.0)
-DATA_DIR = ""
-JSON_LIST = ""
+PIXDIM = (1.0, 1.0, 1.0)
+DATA_DIR = "/bask/projects/p/phwq4930-gbm/Ines/Ovarian/Data/NeOv"
+JSON_LIST = "neov-seg.json"
+LOG_DIR = ""
 PRETRAINED_PATH = "pretrained_models/swinunetr.pt"
 
 
-def main() -> None:
+def main(args) -> None:
     torch.random.manual_seed(0)
 
     # Load model
-    model = SwinUNETR(img_size=IMG_SIZE,
-                      in_channels=IN_CHANNELS,
-                      out_channels=OUT_CHANNELS,
-                      feature_size=FEATURE_SIZE,
-                      drop_rate=DROP_RATE,
-                      attn_drop_rate=ATTN_DROP_RATE,
-                      dropout_path_rate=DROPOUT_PATH_RATE,
-                      use_checkpoint=USE_CHECKPOINT)
+    model = SwinUNETR(img_size=args.roi_size,
+                      in_channels=args.in_channels,
+                      out_channels=args.out_channels,
+                      feature_size=args.feature_size,
+                      drop_rate=args.drop_rate,
+                      attn_drop_rate=args.attn_drop_rate,
+                      dropout_path_rate=args.path_drop_rate,
+                      use_checkpoint=args.grad_checkpoint)
 
-    weights = torch.load(PRETRAINED_PATH)
+    weights = torch.load(args.pretrained_path)
     model.load_state_dict(weights["state_dict"])
 
     # Data Loaders
     train_loader, test_loader = get_train_loader(
-        data_dir=DATA_DIR,
-        json_list=JSON_LIST,
-        batch_size=BATCH_SIZE,
-        space=PIXDIM,
-        roi_size=IMG_SIZE,
-        a_min=A_MIN,
-        a_max=A_MAX,
-        b_min=B_MIN,
-        b_max=B_MAX
+        data_dir=args.data_dir,
+        json_list=args.json_list,
+        batch_size=args.batch_size,
+        space=(args.space_x, args.space_y, args.space_z),
+        roi_size=args.roi_size,
+        a_min=args.a_min,
+        a_max=args.a_max,
+        b_min=args.b_min,
+        b_max=args.b_max
     )
 
     # Loss
-    loss = DiceCELoss(include_background=True, to_onehot_y=True, softmax=True, squared_pred=SQUARED_PRED)
+    loss = DiceCELoss(include_background=True, to_onehot_y=True, softmax=True, squared_pred=args.squared_pred)
 
     # Output transformations
-    post_label = AsDiscrete(to_onehot=OUT_CHANNELS)
-    post_pred = post_pred_transform(OUT_CHANNELS)
+    post_label = AsDiscrete(to_onehot=args.out_channels)
+    post_pred = post_pred_transform(args.out_channels)
 
     # Metric
     dice_acc = DiceMetric(include_background=True, get_not_nans=True)
 
     # Model Inferer for evaluation
-    model_inferer = SwinInferer(model, roi_size=IMG_SIZE)
+    model_inferer = SwinInferer(model, roi_size=args.roi_size)
 
     # Print number of parameters
     pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print("Total parameters count", pytorch_total_params)
 
     # Optimizer
-    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    optimizer = torch.optim.AdamW(model.parameters(),
+                                  lr=args.learning_rate,
+                                  weight_decay=args.weight_decay)
 
     # Scheduler
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=MAX_EPOCHS)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.max_epochs)
 
-    run_training(model=model,
-                 train_loader=train_loader,
-                 test_loader=test_loader,
-                 optimizer=optimizer,
-                 loss_func=loss,
-                 acc_func=dice_acc,
-                 scheduler=scheduler,
-                 max_epochs=MAX_EPOCHS,
-                 batch_size=BATCH_SIZE,
-                 val_every=VAL_EVERY,
-                 model_inferer=model_inferer,
-                 start_epoch=0,
-                 post_label=post_label,
-                 post_pred=post_pred)
+    run_training(
+        model=model,
+        train_loader=train_loader,
+        test_loader=test_loader,
+        optimizer=optimizer,
+        loss_func=loss,
+        acc_func=dice_acc,
+        scheduler=scheduler,
+        max_epochs=args.max_epochs,
+        batch_size=args.batch_size,
+        val_every=args.val_every,
+        model_inferer=model_inferer,
+        start_epoch=0,
+        post_label=post_label,
+        post_pred=post_pred,
+        log_dir=args.log_dir
+    )
 
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args)
