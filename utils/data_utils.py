33a8586abe25221f9b80723433c744ff5e630698
BevisD <bevisd@btinternet.com>
2024-03-12 15:46:14 +0000

    mmap data loader

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,97 +1,102 @@
-# Copyright 2020 - 2022 MONAI Consortium
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import json
 import os
+import sys
+import torch
+
+import numpy as np
+from torch.utils.data import DataLoader
+
 from typing import Sequence
 
-from monai import data
-from monai.data import load_decathlon_datalist
 
-from .transforms import compose_train_transform, compose_test_transform
+class SegmentationDataLoader:
+    def __init__(self, data_dir: str,
+                 json_file: str,
+                 data_list_key: str,
+                 image_key: str = "image",
+                 label_key: str = "label"):
+        self.image_key = image_key
+        self.label_key = label_key
+        self.data_dir = data_dir
 
-__all__ = ["get_train_loader",
-           "get_test_loader"]
+        json_path = os.path.join(data_dir, json_file)
+        with open(json_path, "r") as fp:
+            data = json.load(fp)
+            if data_list_key not in data:
+                raise ValueError(f"key '{data_list_key}' not in data")
+
+            self.data_list = data[data_list_key]
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        # Keep images in disk memory
+        image_path = os.path.join(self.data_dir, self.data_list[idx][self.image_key])
+        label_path = os.path.join(self.data_dir, self.data_list[idx][self.label_key])
+        image = np.load(image_path, mmap_mode="r")
+        label = np.load(label_path, mmap_mode="r")
+
+        data = {
+            self.image_key: image,
+            self.label_key: label
+        }
+        return data
 
 
-def get_train_loader(data_dir: str,
-                     json_list: str,
-                     batch_size: int,
-                     space: Sequence[float] | float,
-                     roi_size: Sequence[int] | int,
-                     a_min: float,
-                     a_max: float,
-                     b_min: float,
-                     b_max: float,
-                     ) -> list[data.DataLoader]:
-    train_transform = compose_train_transform(
-        space=space,
-        a_min=a_min,
-        a_max=a_max,
-        b_min=b_min,
-        b_max=b_max,
-        roi_size=roi_size,
+class SegmentationPatchDataLoader(SegmentationDataLoader):
+    def __init__(self, data_dir: str,
+                 json_file: str,
+                 data_list_key: str,
+                 patch_size: int | Sequence[int],
+                 patch_batch_size: int,
+                 image_key: str = "image",
+                 label_key: str = "label"):
+
+        super().__init__(data_dir=data_dir,
+                         json_file=json_file,
+                         data_list_key=data_list_key,
+                         image_key=image_key,
+                         label_key=label_key)
+
+        self.patch_size = patch_size
+        self.patch_batch_size = patch_batch_size
+
+    def __getitem__(self, idx):
+        data = super().__getitem__(idx)
+        image, label = data[self.image_key], data[self.label_key]
+
+        patches = self.get_patches(image, label, num_patches=self.patch_batch_size)
+
+        return patches
+
+    def get_patches(self, image, label, num_patches=4):
+        slices = [None] * 3
+
+        patches = []
+        for _ in range(num_patches):
+            for i, (image_length, patch_length) in enumerate(zip(image.shape, self.patch_size)):
+                if image_length >= patch_length:
+                    patch_corner = np.random.randint(0, image_length - patch_length + 1)
+                else:
+                    patch_corner = 0
+
+                slices[i] = slice(patch_corner, patch_corner + patch_length)
+
+            image_patch = image[*slices]
+            label_patch = label[*slices]
+
+            patches.append({
+                "image": image_patch,
+                "label": label_patch
+            })
+
+        return patches
+
+
+if __name__ == '__main__':
+    loader = SegmentationDataLoader(
+        data_dir="preprocessed",
+        json_file="neov.json",
+        data_list_key="training",
     )
-    test_transform = compose_test_transform(
-        space=space,
-        a_min=a_min,
-        a_max=a_max,
-        b_min=b_min,
-        b_max=b_max,
-    )
-
-    data_dir = data_dir
-    datalist_json = os.path.join(data_dir, json_list)
-
-    datalist = load_decathlon_datalist(datalist_json, True, "training", base_dir=data_dir)
-    # train_ds = data.Dataset(data=datalist, transform=train_transform)
-    train_ds = data.CacheDataset(
-        data=datalist, transform=train_transform, cache_num=24, cache_rate=1.0, num_workers=None
-    )
-    train_loader = data.DataLoader(
-        train_ds, batch_size=batch_size, shuffle=True, pin_memory=True
-    )
-
-    test_files = load_decathlon_datalist(datalist_json, True, "test", base_dir=data_dir)
-    test_ds = data.Dataset(data=test_files, transform=test_transform)
-    test_loader = data.DataLoader(
-        test_ds, batch_size=1, shuffle=False, pin_memory=True
-    )
-    return [train_loader, test_loader]
-
-
-def get_test_loader(data_dir: str,
-                    json_list: str,
-                    space: Sequence[float] | float,
-                    a_min: float,
-                    a_max: float,
-                    b_min: float,
-                    b_max: float):
-    data_dir = data_dir
-    datalist_json = os.path.join(data_dir, json_list)
-
-    test_transform = compose_test_transform(
-        space=space,
-        a_min=a_min,
-        a_max=a_max,
-        b_min=b_min,
-        b_max=b_max,
-    )
-
-    test_files = load_decathlon_datalist(datalist_json, True, "test", base_dir=data_dir)
-    test_ds = data.Dataset(data=test_files, transform=test_transform)
-    test_loader = data.DataLoader(
-        test_ds,
-        batch_size=1,
-        shuffle=False,
-        pin_memory=True,
-        persistent_workers=True,
-    )
-    return test_loader
